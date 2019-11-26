5ad718fde60dcbd1edbb1153b81dc855b600dedc
Nazar Dranhovskyi <ndranhovskyi@oneviewhealthcare.com>
2019-11-26 16:14:51 +0200

    remove useless usings

--- BlankAppWebViewStripe/BlankAppWebViewStripe/AppConstants.cs
+++ BlankAppWebViewStripe/BlankAppWebViewStripe/AppConstants.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace BlankAppWebViewStripe
+﻿namespace BlankAppWebViewStripe
 {
     public static class AppConstants
     {
